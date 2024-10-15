f6dad7ea28f02c92c836c9168cc6552c87bbcdf2
Hugo <hugo.machabert@etu.univ-lyon1.fr>
2024-10-15 08:28:16 +0200

    fonction connexion

--- admin.php
+++ admin.php
@@ -1,9 +1,7 @@
 <?php require_once('header.php'); ?>
 
 <form action="POST">
-    <input type="text" name="mail" value="" placeholder="Email du compte">
-    <input type="password" name="mdp" value="" placeholder="Mot de passe du compte">
-    <input type="text" name="pseudo" value="" placeholder="Pseudo du compte">
+    <input type="text" name="categorie" value="" placeholder="Catégorie">
 
     <button type="submit" name="btn-creer">Créer</button>
     <button type="submit" name="btn-modifier">Modifier</button>
