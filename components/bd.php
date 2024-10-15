aca781f813794b6ce0aa96e72178e1c21f1048a5
Hugo <hugo.machabert@etu.univ-lyon1.fr>
2024-10-15 10:30:30 +0200

    page création article

--- components/bd.php
+++ components/bd.php
@@ -3,8 +3,8 @@
         //récupération des données
         try {
             $connexion = new PDO('mysql:host=localhost;dbname=blog_s2', 'root', '');
-            $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXEPTION);
-            return $connexion
+            $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $connexion;
         }
         catch (PDOException $e){
             die('Erreur PDO : ' . $e->getMessage());
@@ -12,5 +12,20 @@
         catch (Exception $e){
             die('Erreur Générale : ' . $e->getMessage());
         }
+    }
+
+
+    function getCategory($pdo) {
+        // Prepare the SQL query
+        $sql = 'SELECT id_cat, name_cat FROM CATEGORY';
+    
+        // Prepare and execute the query
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+    
+        // Fetch all results as an associative array
+        $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+        return $categories; // Returns the list of categories
     }
 ?>
